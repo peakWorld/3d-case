4ac7bc2ebe69e613814ed92ae8f7b14f33cc7b92
liuyingfeng <liuyingfeng@58.com>
2021-03-05 19:30:37 +0800

    feat: 魔方开始开发

--- src/pages/rubik/index.tsx
+++ src/pages/rubik/index.tsx
@@ -2,19 +2,33 @@
  * @Author: lyf
  * @Date: 2021-02-01 10:49:32
  * @LastEditors: lyf
- * @LastEditTime: 2021-02-01 15:09:46
+ * @LastEditTime: 2021-03-05 19:10:11
  * @Description: 3D魔方
  * @FilePath: /cook-electron/Users/a58/iworkspace/3d-case/src/pages/rubik/index.tsx
  */
-import React from 'react'
-import { Mesh } from 'three'
-// import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
+import React, { useEffect, useRef } from 'react'
+import { createStats } from '@utils/help'
+import { Vector3 } from 'three'
 // import Stats from 'three/examples/jsm/libs/stats.module.js'
 
+interface Cache {
+  origin: Vector3 /* 魔方原点 */
+  steps: number /* 阶 */
+  len: number /* 阶长 */
+}
+
 const Rubik = () => {
+  const ref = useRef<HTMLDivElement>(null)
+  const cache = useRef<Cache>({ steps: 3, len: 50, origin: new Vector3(0, 0, 0) })
+
+  useEffect(() => {
+    const dom = ref.current as HTMLElement
+    const stats = createStats({ dom })
+
+  }, [])
 
   return (
-    <div>Rubik</div>
+    <div className="rubik"></div>
   )
 }
 
